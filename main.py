5ba287d1bb7d4336ce3e2a4f16ccdaac82957a94
ahmed achour <66746240+17med@users.noreply.github.com>
2022-07-24 20:55:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ from tkinter import *
 
 from pages import loginpage,register,home_page
 
-#window
+
 root = Tk()
-#x=register(root)
+
 x=loginpage(root)
-#x=home_page(root,"admin")
-#x=menu(root)
-#x=historique(root)
 
-"""
-mainpage(root,'800x500')
-maincreation(root)
 
-mainpage(root,'950x650')
-menu(root)"""
 root.mainloop()
